d8a46512c7159882a2853afa400cdeb431d74395
TahirMansuri <tprocks786@gmail.com>
2022-04-11 11:59:47 +0530

    MODIFIED: Employee Payroll App Back End Setup

---
+++ src/main/java/com/infogalaxy/employeepayrollappbackend/EmployeePayrollAppBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.infogalaxy.employeepayrollappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeePayrollAppBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeePayrollAppBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/infogalaxy/employeepayrollappbackend/EmployeePayrollAppBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infogalaxy.employeepayrollappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeePayrollAppBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
