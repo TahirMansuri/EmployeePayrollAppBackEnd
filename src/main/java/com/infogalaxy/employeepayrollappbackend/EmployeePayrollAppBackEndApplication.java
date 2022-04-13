2d710e78ec4c7e1927289be2fd1e29cfcf201b6c
TahirMansuri <tprocks786@gmail.com>
2022-04-13 14:21:14 +0530

    MODIFIED: Employee Payroll App with Different Logging Level Application Properties Files

--- src/main/java/com/infogalaxy/employeepayrollappbackend/EmployeePayrollAppBackEndApplication.java
+++ src/main/java/com/infogalaxy/employeepayrollappbackend/EmployeePayrollAppBackEndApplication.java
@@ -3,13 +3,14 @@ package com.infogalaxy.employeepayrollappbackend;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 @Slf4j
 public class EmployeePayrollAppBackEndApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeePayrollAppBackEndApplication.class, args);
-        log.info("Employee Payroll App Service Started.");
+        ApplicationContext context = SpringApplication.run(EmployeePayrollAppBackEndApplication.class, args);
+        log.info("Employee Payroll App Service Started in {} Environment",context.getEnvironment().getProperty("environment"));
     }
 }
